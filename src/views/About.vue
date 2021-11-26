5b449a0fd2245a4c76ead39eb07215f3a49dd663
Rushan Nazmiev <nazmievjob@gmail.com>
2021-11-26 15:41:58 +0300

    created project

---
+++ src/views/About.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="about"> 
+    <div v-for="item in items" :key="item.id">
+      <div v-if="item.id == catId">
+        <img alt="Vue logo" :src="require(`../assets/${item.link}.jpg`)">
+      </div>
+    </div>
+    <div class="comment">
+      <span>Комментарии:</span>
+      <textarea v-model="message"></textarea>
+      <button @click="add">Добавить</button>
+      <p v-for="comment in comments" :key="comment">
+        {{ comment }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      catId: this.$route.params.id,
+      items: [
+        {id: 1, link: 'cat1', like: false},
+        {id: 2, link: 'cat2', like: false},
+        {id: 3, link: 'cat3', like: false},
+        {id: 4, link: 'cat4', like: false},
+        {id: 5, link: 'cat5', like: false},
+        {id: 6, link: 'cat1', like: false},
+        {id: 7, link: 'cat2', like: false},
+        {id: 8, link: 'cat3', like: false},
+        {id: 9, link: 'cat4', like: false},
+        {id: 10, link: 'cat5', like: false},
+        {id: 11, link: 'cat1', like: false},
+        {id: 12, link: 'cat2', like: false},
+        {id: 13, link: 'cat3', like: false},
+        {id: 14, link: 'cat4', like: false},
+        {id: 15, link: 'cat5', like: false},
+        {id: 16, link: 'cat1', like: false},
+        {id: 17, link: 'cat2', like: false},
+        {id: 18, link: 'cat3', like: false},
+        {id: 19, link: 'cat4', like: false},
+        {id: 20, link: 'cat5', like: false}
+      ],
+      message: '',
+      comments: ['Здорово', 'класс']
+    }
+  },
+
+  methods:{
+    add(){
+      this.comments.push(this.message)
+      this.message = ''
+    }
+  },
+
+}
+</script>
+
+<style>
+  .about {
+    width: 50%;
+  }
+  .about img {
+    width: 100%;
+    border-radius: 15px;
+  }
+  .comment {
+    padding-top: 50px;
+  }
+  span {
+    display: block;
+    padding-bottom: 10px;
+    font-size: 30px;
+  }
+  textarea {
+    resize: none;
+    width: 50%;
+    height: 140px;
+    font-size: 20px;
+  }
+  button {
+    display: block;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="wrapper">
+    <div class="item" 
+         v-for="item in items" 
+         :key="item.id"
+         @click="$router.push({ name: 'about', params: { id: item.id } })">
+      <img alt="Vue logo" :src="require(`../assets/${item.link}.jpg`)">
+      <div class="icon-box" @click.stop="addLike(item)">
+        <svg  viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path :class="{liked:item.like}" d="M10.5 0.000120477C12.5988 0.0108192 13.9898 1.78957 14 3.98706C14 8.01306 7.00002 12 7.00002 12C7.00002 12 0 7.95444 0 3.98706C0 1.78515 1.39688 0.000120477 3.5 0.000120477C4.78739 -0.0103912 5.83333 0.6 7.00002 1.79813C8.16667 0.6 9.21432 -0.00313402 10.5 0.000120477Z" fill="#000"/>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      items: [
+        {id: 1, link: 'cat1', like: false},
+        {id: 2, link: 'cat2', like: false},
+        {id: 3, link: 'cat3', like: false},
+        {id: 4, link: 'cat4', like: false},
+        {id: 5, link: 'cat5', like: false},
+        {id: 6, link: 'cat1', like: false},
+        {id: 7, link: 'cat2', like: false},
+        {id: 8, link: 'cat3', like: false},
+        {id: 9, link: 'cat4', like: false},
+        {id: 10, link: 'cat5', like: false},
+        {id: 11, link: 'cat1', like: false},
+        {id: 12, link: 'cat2', like: false},
+        {id: 13, link: 'cat3', like: false},
+        {id: 14, link: 'cat4', like: false},
+        {id: 15, link: 'cat5', like: false},
+        {id: 16, link: 'cat1', like: false},
+        {id: 17, link: 'cat2', like: false},
+        {id: 18, link: 'cat3', like: false},
+        {id: 19, link: 'cat4', like: false},
+        {id: 20, link: 'cat5', like: false}
+      ]
+    }
+  },
+  methods: {
+    addLike(i) {
+      i.like = !i.like
+    }
+  }
+}
+</script>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .item {
+    position: relative;
+    border: 1px solid #ccc;
+    padding: 3px;
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 10px 10px 0px;
+    width: 100%;
+    max-width: 300px;
+  }
+  .item .icon-box {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    cursor: pointer;
+  }
+  .icon-box svg {
+    width: 30px;
+  }
+  .icon-box svg .liked {
+    fill: #42b983;
+  }
+  .item img {
+    width: 100%;
+    border-radius: 15px;
+  }
+</style>
